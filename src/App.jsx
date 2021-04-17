2482e77c9fa0288757dad8f230e20a9daf31e519
Tahsin Ahmed <tahsinahmed95@gmail.com>
2021-04-17 01:08:59 -0400

    added more styling and new fonts and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './customStyles.css';
+import { Nav } from './components/Nav.jsx';
+import { Projects } from './components/Projects.jsx';
+import { About } from './components/About.jsx';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const App = () => {
 	return (
-		<>
-			<h1 className="text-4xl text-white bg-blue-700">Hello Tahsin!</h1>
-		</>
+		<Router>
+			<Nav />
+			<Route path="/about" component={About} />
+			<Route path="/projects" component={Projects} />
+		</Router>
 	);
 };
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Nav = () => {
+	return (
+		<div className="flex justify-around items-center text-pink bg-darkpurple font-poppins">
+			<h1>Tahsin Ahmed</h1>
+		</div>
+	);
+};
