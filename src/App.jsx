7a12d70d358157589ffa4067e55320e913bb60bc
Tahsin Ahmed <tahsinahmed95@gmail.com>
2021-04-16 22:41:25 -0400

    pls

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const App = () => {
 	return (
 		<>
-			<h1 className="text-4xl text-white bg-blue-700">Hello Tahsin</h1>
+			<h1 className="text-4xl text-white bg-blue-700">Hello Tahsin!</h1>
 		</>
 	);
 };
